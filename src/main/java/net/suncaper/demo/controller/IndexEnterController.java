aabc2fba0877f7f4a96f26060e0f5b3f3bcfc8b7
fanyang <1763089574@qq.com>
2021-01-26 11:16:34 +0800

    初版项目，只有首页和登陆页

---
+++ src/main/java/net/suncaper/demo/controller/IndexEnterController.java
@@ -0,0 +1,20 @@
+package net.suncaper.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexEnterController {
+    @RequestMapping("/EnterToCompany")
+    public String EnterToCompany(){
+        return "CompanyIndex";
+    }
+    @RequestMapping("/EnterToWorker")
+    public String EnterToWorker(){
+        return "WorkerIndex";
+    }
+    /*@RequestMapping("/EnterToWorker")
+    public String EnterToWorker(){
+        return "CompanyIndex";
+    }*/
+}
