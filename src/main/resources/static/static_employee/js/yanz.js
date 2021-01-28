ecda288e17eef19fbcc6ca8b911b7210fe564871
DengFist <648427944@qq.com>
2021-01-28 23:52:15 +0800

    1.前端验证码效果改为VUE 2。修改了static_company和static_employee下的引用。这两个文件夹下的文件有些名字一样，但是内容不同。

--- src/main/resources/static/static_employee/js/yanz.js
+++ src/main/resources/static/static_employee/js/yanz.js
@@ -1,41 +1,26 @@
-var code_time = 90;
 
-function time_inter() {
-    $('.code-btn').addClass('code-btn-gray');
-    var timeflag = setInterval(function() {
-        $('.code-btn-gray').html("" + code_time + "s后获取").css({
-            "cursor": "no-",
-            "color": "#999"
-        });
-        code_time--;
-        if (code_time == 0) {
-            clearInterval(timeflag);
-            $('.code-btn-gray').html("获取验证码").css({
-                "cursor": "pointer",
-                "color": "#a07941"
-            });
-            $('.code-btn').removeClass('code-btn-gray');
+new Vue({
+    el:".get-code",
+    data:{
+        show:true,
+        code_time:5
+    },
+    methods:{
+        time1:function (){
+            this.code_time=this.code_time-1;
+            console.log(this.code_time);
+            if(this.code_time==0){
+                clearInterval(this.time_count);
+                this.time_count = null;
+                this.code_time=5;
+                this.show=!this.show;
+            }
+        },
+        check_code:function (){
+            this.show=!this.show;
+
+            this.time_count = setInterval(this.time1,1000);
         }
-    }, 1000)
-
-}
-//获取验证码点击事件
-
-$(document).ready(function(){ 
-    $('.code-btn').on('click', function() {
-    if ($(this).hasClass('code-btn-gray')) {
-        return false;
     }
-    var tel_num = $('#tel_num').val();
-    if (tel_num && tel_num.length == 11) {
-        time_inter(); //按钮倒计时
-    } else {
-        alert('请输入正确手机号码')
-    }
-})  
-
-
-});
-
-
+})
 
